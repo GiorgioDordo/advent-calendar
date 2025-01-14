9a49882204f1746cb7c08e91256d6fe63759c070
GiorgioDordo <djole6789@gmail.com>
2025-01-14 20:58:46 +0100

    Enhance calendar and modal functionality with improved event handling and styling

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ import { source } from './source.js';
 
 console.log(source);
 
+// prendo il calendario dal DOM
 const calendar = document.getElementById("calendar");
-const modal = document.getElementById("myModal");
-const calendarDay = document.getElementsByClassName("calendar-day");
 
+// creo una cella del calendario con il contenuto passato come parametro 
 const createCell = (content = "") => {
+    // creo un div con la classe calendar-day 
     const cell = document.createElement("div");
     cell.classList.add("calendar-day");
+    // aggiungo il contenuto passato come parametro al div
     cell.append(content)
     return cell;
 }
@@ -16,7 +18,6 @@ const createCell = (content = "") => {
 const createModal = (content = "") => {
     const modal = document.createElement("div");
     modal.classList.add("modal");
-    modal.setAttribute("id", "myModal");
     modal.append(content);
     return modal;
 }
@@ -29,43 +30,55 @@ const createContent = (content = "") => {
 }
 
 const createCloseBtn = (content = "") => {
-    const span = document.createElement("div");
-    span.classList.add("close");
+    const span = document.createElement("button");
+    span.classList.add("close", "btn");
     span.append(content);
     return span;
 }
 
+// faccio un ciclo per creare le celle del calendario e aggiungere il contenuto ad ogni cella  
 for (let i = 1; i <= source.length; i++) {
 
+    // creo le celle e le aggiungo come figlie al calendario
     const cell = createCell();
     calendar.appendChild(cell);
 
-    const modal = createModal();
-    body.appendChild(modal);
+    // aggiungo il contenuto ad ogni singola cella facendo un ciclo sull'array source
+    cell.innerHTML = `<img class="icons" src="images/icons/${source[i - 1].icon}.png" />` + `<p>${i}<p>`;
 
+    // creo un evento, quando si clicca sulla cella si apre la modale
+    cell.addEventListener('click', () => {
+        // lo stile della modale diventa flex
+        modal.style.display = "flex";
+        console.log("open");
+    });
+
+    // creo la modale e la aggiungo come figlia al calendario
+    const modal = createModal();
+    calendar.appendChild(modal);
+
+    // creo il contenuto della modale e lo aggiungo come figla alla maodale
     const modalContent = createContent();
     modal.appendChild(modalContent);
 
+    // se l'elemento dell'array è un'immagine, aggiungo un tag img con la classe gif e l'url dell'immagine, se è un testo aggiungo un tag p con il testo
+    if (source[i - 1].type === "image") {
+        modalContent.innerHTML = `<img class="gif" src="${source[i - 1].url}" />`;
+    } else {
+        modalContent.innerHTML = `<p>${source[i - 1].text}</p>`;
+    }
+
+
+    // creo un bottone per chiudere la modale e lo aggiungo come figlio i modalContent
     const span = createCloseBtn();
-    modal.appendChild(span);
+    modalContent.appendChild(span);
 
-    cell.addEventListener('click', () => {
-        modal.style.display = "block";
-        console.log("open", i);
-    });
+    // aggiungo il testo al bottone
+    span.innerHTML = "Close";
 
+    // creo un evento, quando si clicca sul bottone la modale si chiude
     span.addEventListener('click', () => {
         modal.style.display = "none";
         console.log("close", i);
     });
-
-    cell.innerHTML = `<img src="images/icons/${source[i - 1].icon}.png" />` + `<p>${i}<p>`;
-
-    span.innerHTML = "&times;";
-
-    if (source[i - 1].type === "image") {
-        modalContent.innerHTML = `<img src="${source[i - 1].url}" />`;
-    } else {
-        modalContent.innerHTML = `<p>${source[i - 1].text}</p>`;
-    }
 }
